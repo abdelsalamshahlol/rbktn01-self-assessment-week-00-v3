aeb247b244ddd66a223183be44529d83eb464ca7
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-11-20 09:39:47 +0100

    working on recusion

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -56,7 +56,65 @@
 */
 
 
-var filterFamilyMembers = function (familyTree, truthTest) {
-  // All your code in this function body
+var familyTree = {
+    'firstName': 'Beth',
+    'lastName': 'Johnson',
+    'location': 'San Francisco',
+    'children': [{
+        'firstName': 'Beth Jr.',
+        'lastName': 'Johnson',
+        'location': 'Berkeley',
+        'children': [{
+            'firstName': 'Smitty',
+            'lastName': 'Won',
+            'location': 'Beijing',
+            'children': []
+        }]
+    }, {
+        'firstName': 'Joshie',
+        'lastName': 'Wyattson',
+        'location': 'Berkeley',
+        'children': []
+    }]
 };
+
+clear();
+
+var livesInBerkeley = function(familyMember) {
+    return familyMember.location === 'Berkeley';
+}
+
+var filterFamilyMembers = function(familyTree, check) {
+    var arr = Object.values(familyTree);
+    var result = [];
+
+    function processArr(arr) {
+        if (arr.length === 0) {
+            return result;
+        }
+        //        console.log(arr[0], Object.assign({},arr[0]))
+        //        if(check){
+        console.log(arr[0])
+        if (Array.isArray(arr[0])) {
+            console.log(arr[0])
+            result.push(processArr(arr[0]))
+        } else {
+            if (check(arr[0])) {
+                var fullname = arr[0][1] + arr[0][2]
+                result.push(fullname)
+            }
+        }
+
+        return processArr(arr.slice(1));
+
+    }
+
+    //     console.log(arr)
+
+    return processArr(arr)
+}
+
+console.log(filterFamilyMembers(familyTree, livesInBerkeley))
+// returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
+
 
